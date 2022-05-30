c9677e001dd7cb9dafc3624641b69b4d791bdb0a
Jonas-Grossi <jonasgrossicc@gmail.com>
2022-05-30 19:33:32 -0300

    layout exemple

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,15 @@
+export const PostCard =(props)=>{
+const post = props
+return(
+
+<div className="post">
+          <img src={post.cover} alt={post.title}></img>
+        <div key={post.id} className="post-content">
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+     </div>
+
+)
+
+}
