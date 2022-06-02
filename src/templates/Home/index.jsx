5cbd17c5dcfd76fc7927e02eb0d966916309cd0c
Jonas-Grossi <jonasgrossicc@gmail.com>
2022-06-02 09:27:30 -0300

    search 1.0

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,73 @@
+import {Component} from 'react';
+import './styles.css';
+import { Posts } from '../../components/Posts';
+import {loadPosts} from '../../utils/load-posts';
+
+ export class Home extends Component {
+  
+  
+      state={
+        posts:[],
+        allPosts:[],
+        page:0,
+        postsPerPage:9,          
+        searchValue:''    
+      
+      }
+    
+        
+    
+          
+   async componentDidMount(){
+        await this.loadPosts()
+      }
+  loadPosts=async ()=>{
+    const {page,postsPerPage}= this.state
+    const postsAndPhotos = await loadPosts()
+    this.setState({
+      posts: postsAndPhotos.slice(page,postsPerPage),
+      allPosts: postsAndPhotos,
+    })
+    }
+    loadMorePosts = () =>{
+      const {
+        page,
+        postsPerPage,
+        allPosts,
+        posts
+      } = this.state
+        const nextPage = page + postsPerPage
+        const nextPosts = allPosts.slice(nextPage,nextPage+postsPerPage)
+        posts.push(...nextPosts)
+        this.setState({posts,page:nextPage})
+      }
+  handleChange=(e)=>{
+const {value} = e.target;
+this.setState({searchValue:value})
+
+  }
+  render() {
+    const {posts,page,postsPerPage,allPosts,searchValue} = this.state
+    const noMorePosts = page + postsPerPage >=allPosts.length;
+    return ( 
+    <section className="container">
+      <input 
+      onChange={this.handleChange}
+      value={searchValue}
+      type="search"/>
+      <br/><br/><br/>
+      <Posts posts={posts}/>
+       <div className="button-container">
+       <button 
+       disabled ={noMorePosts} 
+       className="button" 
+       onClick={this.loadMorePosts}>Load more posts</button> 
+       </div> 
+    </section>
+  
+  
+  )
+  }
+}
+
+export default Home;
